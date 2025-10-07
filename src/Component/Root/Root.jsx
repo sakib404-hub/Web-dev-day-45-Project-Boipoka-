98983415f30ba825a75481d5aa9b1493d9bbcaa4
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-08 02:36:22 +0600

    the basic synax and installed daisy ui, tailwindcss and react router

---
+++ src/Component/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div className='border'>
+            This is the Root File!
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider, } from "react-router";
+import Root from './Component/Root/Root.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
