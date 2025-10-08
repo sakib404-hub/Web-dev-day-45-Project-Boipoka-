5bc54bf38f9a8a74e66060aa7028d1c473694702
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-10-08 14:54:24 +0600

    adding the functionality of sorting by rating and page

--- src/Component/ReadList/ReadList.jsx
+++ src/Component/ReadList/ReadList.jsx
@@ -8,6 +8,30 @@ const ReadList = () => {
 
     const [readList, setReadlist] = useState([]);
     const [wishList, setWishList] = useState([]);
+    // state for the sort 
+    const [sort, setSort] = useState('');
+    const [open, setOpen] = useState(false);
+
+    const handleSorting = (type) => {
+        setSort(type);
+
+        if (type === 'pages') {
+            const sortedByPageReadList = [...readList].sort((a, b) => a.totalPages - b.totalPages)
+            setReadlist(sortedByPageReadList);
+
+            const sortedByPageWishList = [...wishList].sort((a, b) => a.totalPages - b.totalPages)
+            setWishList(sortedByPageWishList);
+
+        }
+        if (type === 'ratings') {
+            const sortedByRatingList = [...readList].sort((a, b) => b.rating - a.rating)
+            setReadlist(sortedByRatingList);
+
+            const sortedByRatingWishList = [...wishList].sort((a, b) => a.rating - b.rating)
+            setWishList(sortedByRatingWishList);
+        }
+
+    }
 
     const bookData = useLoaderData();
     // console.log(bookData);
@@ -28,15 +52,24 @@ const ReadList = () => {
 
     return (
         <div>
-            <div className='flex items-center justify-center'>
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn m-1">SortBy</div>
-                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                        <li><a>Pages</a></li>
-                        <li><a>Rating</a></li>
-                    </ul>
+            <div className={`dropdown ${open ? 'dropdown-open' : ''} flex items-center justify-center py-5`}>
+                <div
+                    tabIndex={0}
+                    role="button"
+                    className="btn m-1"
+                    onClick={() => setOpen(!open)}
+                >
+                    Sort By: {sort || 'Select'}
                 </div>
+                <ul
+                    tabIndex={0}
+                    className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm"
+                >
+                    <li><a onClick={() => { handleSorting('pages'); setOpen(false); }}>Pages</a></li>
+                    <li><a onClick={() => { handleSorting('ratings'); setOpen(false); }}>Ratings</a></li>
+                </ul>
             </div>
+
             < div className="tabs tabs-lift" >
                 <input type="radio" name="my_tabs_3" className="tab" aria-label="ReadList" />
                 <div className="tab-content bg-base-100 border-base-300 p-6 space-y-6">
